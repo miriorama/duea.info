9b2fe522bfa8c865098abcd5adbdf4944461095c
Daniele Caraglio <daniele.caraglio@gmail.com>
2024-07-26 15:42:38 +0200

    bug fix

--- js/cucuna.js
+++ js/cucuna.js
@@ -3,16 +3,16 @@ var CUCUNA = (function(){
         player: null,
         currentTrack: null,
         trackList:{
-            'cucuna': {id:'cucuna',title:'Cucunà', lato:'A'},
-            'bardo': {id:'bardo',title:'Bardo', lato:'A'},
-            'mazurka': {id:'mazurka',title:'Mazurka J. Da Rous', lato:'A'},
-            'la-marcia-di-biule': {id:'la-marcia-di-biule',title:'La marcha di bioule', lato:'A'},
-            'bobo': {id:'bobo',title:'Bobò', lato:'A'},
-            'margot': {id:'margot',title:'Margot', lato:'B'},
-            'rigolet': {id:'rigolet',title:'Rigolet', lato:'B'},
-            'espirit-libre': {id:'espirit-libre',title:'Esprit libre', lato:'B'},
-            'aura': {id:'aura',title:'Aura', lato:'B'},
-            'bobo-remix': {id:'bobo-remix',title:'Bobò - Madaski Remix', lato:'B'},
+            'cucuna': {id:'cucuna',title:'Cucunà', lato:'a'},
+            'bardo': {id:'bardo',title:'Bardo', lato:'a'},
+            'mazurka': {id:'mazurka',title:'Mazurka J. Da Rous', lato:'a'},
+            'la-marcia-di-biule': {id:'la-marcia-di-biule',title:'La marcha di bioule', lato:'a'},
+            'bobo': {id:'bobo',title:'Bobò', lato:'a'},
+            'margot': {id:'margot',title:'Margot', lato:'b'},
+            'rigolet': {id:'rigolet',title:'Rigolet', lato:'b'},
+            'espirit-libre': {id:'espirit-libre',title:'Esprit libre', lato:'b'},
+            'aura': {id:'aura',title:'Aura', lato:'b'},
+            'bobo-remix': {id:'bobo-remix',title:'Bobò - Madaski Remix', lato:'b'},
 
         }
     };
